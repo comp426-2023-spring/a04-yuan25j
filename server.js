98722ecf1f0da54382a40b438c8267242b2653d7
John Yuan <89277444+yuan25j@users.noreply.github.com>
2023-03-21 11:29:49 -0400

    updates

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ import {rps, rpsls} from "./lib/rpsls.js"
 import minimist from 'minimist'
 import express from 'express'
 
-let argv = minimist(process.argv.slice(2));
+var argv = minimist(process.argv.slice(2));
 const port = argv.port || 5000;
 
-let app = express();
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/app', (req, res) => {
     res.status(200).send('200 OK').end();
 });
+
 
 app.get('/app/rps', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
